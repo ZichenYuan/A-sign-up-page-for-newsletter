4a8e4b16e698f8a6f881c8549c79b418a43f80ca
ZichenYuan <y.zichen@wustl.edu>
2022-06-13 18:58:42 -0700

    first update

--- app.js
+++ app.js
@@ -62,10 +62,10 @@ app.post("/failure", function(req,res){
 });
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT ||3000, function(){
   console.log("the server is running on port 3000");
 });
-
+//work both on heroku and local 3000
 
 
 // API Key
